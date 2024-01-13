38df55aa46abd7e4e92f3e6006a255f80fde23f0
Xue Shirley <shirleyfang118@gmail.com>
2024-01-13 15:00:52 -0500

    updated selenium test

--- src/main/java/com/qa/opencart/pages/SeleniumTest.java
+++ src/main/java/com/qa/opencart/pages/SeleniumTest.java
@@ -3,12 +3,10 @@ package com.qa.opencart.pages;
 public class SeleniumTest {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
+
 		int i=20;
 		
-=======
-		int i=10;
->>>>>>> 7d7982150f22ed38fe04f69539dc3b10b82e3633
+
 
 	}
 
