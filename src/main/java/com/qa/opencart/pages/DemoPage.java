8d5ea4d2c8c72e96680f2e35c0b50fa1f9dcf869
Xue Shirley <shirleyfang118@gmail.com>
2024-01-13 14:15:51 -0500

    updated

---
+++ src/main/java/com/qa/opencart/pages/DemoPage.java
@@ -0,0 +1,18 @@
+package com.qa.opencart.pages;
+
+public class DemoPage {
+	
+	int total=10;
+
+	public void demo() {
+		
+
+		System.out.println("Naveen - Demo");
+		if(true) {
+			System.out.println("bye");
+		}
+
+	}
+	
+	
+}
---
+++ src/main/java/com/qa/opencart/pages/SeleniumTest.java
@@ -0,0 +1,11 @@
+package com.qa.opencart.pages;
+
+public class SeleniumTest {
+
+	public static void main(String[] args) {
+		int i=20;
+		
+
+	}
+
+}
